8f711179c74c94f88d9ba466d8cb6dde708e991e
Ubuntu <ubuntu@ip-10-0-13-58.ec2.internal>
2026-02-05 12:29:46 +0000

    Modificada base de datos

--- sql/database.sql
+++ sql/database.sql
@@ -24,25 +24,25 @@ INSERT INTO usuario (nombre_usuario, contrasena, correo, nombre, apellido, edad,
 
 
 CREATE TABLE pokemons (
-  pokemons_id INT UNSIGNED AUTO_INCREMENT,
-  nº_pokedex INT UNIQUE PRIMARY KEY,
+  pokemons_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  nº_pokedex INT UNIQUE,
   nombre VARCHAR(50) NOT NULL UNIQUE,
   tipo VARCHAR(150) NOT NULL,
   descripcion VARCHAR(500),
   habilidad VARCHAR(150) NOT NULL UNIQUE,
   debilidad VARCHAR(150) NOT NULL,
-  region ENUM('Kanto', 'Johto', 'Hoenn', 'Sinnoh', 'Unova', 'Kalos', 'Alola', 'Galar', 'Hisui', 'Paldea'),
+  region ENUM('Kanto', 'Johto', 'Hoenn', 'Sinnoh', 'Unova', 'Kalos', 'Alola', 'Galar', 'Hisui', 'Paldea', 'Teselia'),
   generacion ENUM('1º', '2º', '3º', '4º', '5º', '6º', '7º', '8º', '9º'),
   creacion TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO pokemons (pokemons_nº, nombre, tipo, descripcion, habilidad, debilidad, region, generacion) VALUES(25, 'Pikachu', 'electrico', 'Cuando se enfada, descarga electricidad por su cola.', 'electricidad estatica', 'tierra', 'Kanto', '1º');
-INSERT INTO pokemons (pokemons_nº, nombre, tipo, descripcion, habilidad, debilidad, region, generacion) VALUES(4, 'Charmander', 'fuego', 'Su cola arde con una llama que refleja su estado de ánimo', 'Mar Llamas', 'agua', 'Kanto', '1º');
-INSERT INTO pokemons (pokemons_nº, nombre, tipo, descripcion, habilidad, debilidad, region, generacion) VALUES(300, 'Skitty', 'normal', 'Le encanta mover cosas e ir detras de ellas', 'Gran encanto y Normalidad', 'lucha', 'Hoenn', '2º');
-INSERT INTO pokemons (pokemons_nº, nombre, tipo, descripcion, habilidad, debilidad, region, generacion) VALUES(299, 'Nosepass', 'roca', 'Tiene una nariz brujula increble', 'Robustez e Iman', 'agua, planta, lucha, tirra y acero', 'Hoenn', '2º');
-INSERT INTO pokemons (pokemons_nº, nombre, tipo, descripcion, habilidad, debilidad, region, generacion) VALUES(555, 'Darmanitan', 'fuego', 'Cuando resulta gravemente herido, su cuerpo se endurece y medita', 'Potencia bruta', 'agua tierra y roca', 'Teselia', '5º');
-INSERT INTO pokemons (pokemons_nº , nombre , tipo , descripcion , habilidad , debilidad , region , generacion ) VALUES(150 ,  'Mewtwo',  'psiquico',  'Se dice que su ADN es 100% puro',  'Presion',  'bicho, fantasma y siniestro',  'Kanto', '1º');
-INSERT INTO pokemons (pokemons_nº, nombre, tipo, descripcion, habilidad, debilidad, region, generacion) VALUES(1000, 'Gholdengo', 'acero/fantasma', 'Se dice que su cuerpo esta formado por 1000 monedas', 'Cuerpo autea', 'fuego, tierra, fantasma y siniestro', 'Paldea', '9º');
-INSERT INTO pokemons (pokemons_nº, nombre, tipo, descripcion, habilidad, debilidad, region, generacion) VALUES(602, 'Tynamo', 'electrico', 'La electricidad de uno de uno solo es insignificante', 'Levitacion', 'Tierra', 'Teselia', '5º');
-INSERT INTO pokemons (pokemons_nº, nombre, tipo, descripcion, habilidad, debilidad, region, generacion) VALUES(393, 'Piplup', 'agua', 'No se le da bien andar', 'Torrente', 'Planta y electrico', 'Sinnoh', '4º');
-INSERT INTO pokemons (pokemons_nº, nombre, tipo, descripcion, habilidad, debilidad, region, generacion) VALUES(1001, 'Wo-Chien', 'siniestro/planta', 'Absorbe la energia de la vegetación marchitando los bosques', 'Tablilla debacle', 'fuego hielo lucha veneno volador bicho y hada ',  'Paldea', '9º');
+INSERT INTO pokemons (nº_pokedex, nombre, tipo, descripcion, habilidad, debilidad, region, generacion) VALUES(25, 'Pikachu', 'electrico', 'Cuando se enfada, descarga electricidad por su cola.', 'electricidad estatica', 'tierra', 'Kanto', '1º');
+INSERT INTO pokemons (nº_pokedex, nombre, tipo, descripcion, habilidad, debilidad, region, generacion) VALUES(4, 'Charmander', 'fuego', 'Su cola arde con una llama que refleja su estado de ánimo', 'Mar Llamas', 'agua', 'Kanto', '1º');
+INSERT INTO pokemons (nº_pokedex, nombre, tipo, descripcion, habilidad, debilidad, region, generacion) VALUES(300, 'Skitty', 'normal', 'Le encanta mover cosas e ir detras de ellas', 'Gran encanto y Normalidad', 'lucha', 'Hoenn', '2º');
+INSERT INTO pokemons (nº_pokedex, nombre, tipo, descripcion, habilidad, debilidad, region, generacion) VALUES(299, 'Nosepass', 'roca', 'Tiene una nariz brujula increble', 'Robustez e Iman', 'agua, planta, lucha, tirra y acero', 'Hoenn', '2º');
+INSERT INTO pokemons (nº_pokedex, nombre, tipo, descripcion, habilidad, debilidad, region, generacion) VALUES(555, 'Darmanitan', 'fuego', 'Cuando resulta gravemente herido, su cuerpo se endurece y medita', 'Potencia bruta', 'agua tierra y roca', 'Teselia', '5º');
+INSERT INTO pokemons (nº_pokedex , nombre , tipo , descripcion , habilidad , debilidad , region , generacion ) VALUES(150 ,  'Mewtwo' ,  'psiquico' ,  'Se dice que su ADN es 100% puro' ,  'Presion' ,  'bicho,fantasma y siniestro' ,  'Kanto','1º');
+INSERT INTO pokemons (nº_pokedex , nombre , tipo , descripcion , habilidad , debilidad , region , generacion ) VALUES(1000,'Gholdengo','acero/fantasma','Se dice que su cuerpo esta formado por 1000 monedas','Cuerpo autea','fuego tierra fantasma y siniestro','Paldea','9º');
+INSERT INTO pokemons (nº_pokedex, nombre, tipo, descripcion, habilidad, debilidad, region, generacion) VALUES(602, 'Tynamo', 'electrico', 'La electricidad de uno de uno solo es insignificante', 'Levitacion', 'Tierra', 'Teselia', '5º');
+INSERT INTO pokemons (nº_pokedex, nombre, tipo, descripcion, habilidad, debilidad, region, generacion) VALUES(393, 'Piplup', 'agua', 'No se le da bien andar', 'Torrente', 'Planta y electrico', 'Sinnoh', '4º');
+INSERT INTO pokemons (nº_pokedex, nombre, tipo, descripcion, habilidad, debilidad, region, generacion) VALUES(1001, 'Wo-Chien', 'siniestro/planta', 'Absorbe la energia de la vegetación marchitando los bosques', 'Tablilla debacle', 'fuego hielo lucha veneno volador bicho y hada ',  'Paldea', '9º');
